317796c69654642bae495c5dce041b8fd4f2e760
sameernangare <sameernangare63@gmail.com>
2024-02-06 15:40:01 +0530

    feat: add UserSidebar, SidebarStyles

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,4 +1,5 @@
 import Hero from "../components/Hero";
+import UserSidebar from "../components/UserSidebar";
 
 // Background image of home page
 import homeImg from "../assets/homeImg.jpg";
@@ -6,16 +7,18 @@ import homeImg from "../assets/homeImg.jpg";
 function Home() {
   return (
     <>
-      {/* Rendering the Hero component with specified props (Parent -- Home) (Child -- Hero) */}
-      <Hero
-        cName="hero"
-        heroImg={homeImg} // Image for the hero section
-        title="Your Journey Your Story" // Title for the hero section
-        text="Choose Your Favourite Destination." // Text content for the hero section
-        url="/bookflight"
-        btnClass="show" // CSS class for styling the button
-        btnText="Book Tickets" // Text content for the button
-      />
+      <UserSidebar>
+        {/* Rendering the Hero component with specified props (Parent -- Home) (Child -- Hero) */}
+        <Hero
+          cName="hero"
+          heroImg={homeImg} // Image for the hero section
+          title="Your Journey Your Story" // Title for the hero section
+          text="Choose Your Favourite Destination." // Text content for the hero section
+          url="/bookflight"
+          btnClass="show" // CSS class for styling the button
+          btnText="Book Tickets" // Text content for the button
+        />
+      </UserSidebar>
     </>
   );
 }
