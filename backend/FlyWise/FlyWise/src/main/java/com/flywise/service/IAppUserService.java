be9039f7da8459e6bdc3608dda895ff21663079d
D1-80706-RohanMane <rohhaanmane@gmail.com>
2024-02-11 09:37:34 +0530

    Add Pojo Passanger, Booking, Flight Relation

--- backend/FlyWise/FlyWise/src/main/java/com/flywise/service/IAppUserService.java
+++ backend/FlyWise/FlyWise/src/main/java/com/flywise/service/IAppUserService.java
@@ -1,18 +1,18 @@
-package com.flywise.service;
-
-import java.util.List;
-
-import com.flywise.dto.AppUserDto;
-import com.flywise.dto.CustomDto;
-import com.flywise.exception.UserException;
-import com.flywise.pojos.AppUser;
-
-
-
-public interface IAppUserService {
-	
-	public List<AppUser> fetchAllUsers();
-	void registerUser(AppUserDto appUserDto) throws UserException;
-	 
-//	CustomDto confirmBooking(int bid);
-}
+//package com.flywise.service;
+//
+//import java.util.List;
+//
+//import com.flywise.dto.AppUserDto;
+//import com.flywise.dto.CustomDto;
+//import com.flywise.exception.UserException;
+//import com.flywise.pojos.AppUser;
+//
+//
+//
+//public interface IAppUserService {
+//	
+//	public List<AppUser> fetchAllUsers();
+//	void registerUser(AppUserDto appUserDto) throws UserException;
+//	 
+////	CustomDto confirmBooking(int bid);
+//}
